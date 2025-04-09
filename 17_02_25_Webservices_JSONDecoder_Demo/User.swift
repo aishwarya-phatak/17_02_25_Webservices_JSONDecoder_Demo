48bd0d9b10048b5e8022938dba1ce5d414603fac
aishwarya <ashp17star@gmail.com>
2025-04-09 16:09:00 +0530

    Webservices using json decoder

--- 17_02_25_Webservices_JSONDecoder_Demo/User.swift
+++ 17_02_25_Webservices_JSONDecoder_Demo/User.swift
@@ -7,8 +7,8 @@
 
 struct User : Decodable{
     var id : Int
-    var email : String
-    var first_name : String
+    var email : String?
+    var first_name : String?
     var last_name : String
     var avatar : String
 }
